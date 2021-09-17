32131aa09537f180e5d42f877205a3a20edf921a
rasool <rsool_2001@yahoo.com>
2021-09-17 12:48:06 -0500

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+console.log(document.title);
+
+function scratchAnswer() {
+    var answerElement = this.parentNode.children[1].children[1].children[0];
+    
+    if (answerElement.style.textDecoration == "line-through") {
+        answerElement.style.textDecoration = "none";
+    } else {
+        answerElement.style.textDecoration = "line-through";
+    }
+
+    return false;
+}
+
+if (document.title.includes("Quiz")) {
+    var answersList = document.getElementsByClassName("answer");
+
+    for (var i = 0; i < answersList.length; i++) {
+        let scratchButton = document.createElement("button");
+        scratchButton.type = "button";
+        scratchButton.style = "background: rgb(30, 71, 123); color: white; text-align: center; padding: 2px 3px; font-size: small; border: none;";
+        scratchButton.innerText = "Scratch"
+        scratchButton.onclick = scratchAnswer;
+        answersList[i].appendChild(scratchButton);
+    }
+}
